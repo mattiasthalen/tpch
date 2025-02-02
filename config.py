1f8fa29d04afebdd44dca9815d96d98e423681d4
Mattias Thalén <bitter-polders0x@icloud.com>
2025-02-02 23:38:07 +0100

    Init SQLMesh

---
+++ config.py
@@ -0,0 +1,59 @@
+import getpass
+import subprocess
+
+from dotenv import load_dotenv
+from sqlmesh.core.config import (
+    Config,
+    ModelDefaultsConfig,
+    GatewayConfig,
+    DuckDBConnectionConfig,
+    NameInferenceConfig,
+    CategorizerConfig,
+    PlanConfig,
+    AutoCategorizationMode
+)
+
+load_dotenv()
+
+def get_current_branch():
+    try:
+        branch_name = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).strip().decode('utf-8')
+        return branch_name
+    except Exception as e:
+        print(f"Error getting current branch: {e}")
+        return None
+
+current_user = getpass.getuser()
+branch = get_current_branch() or 'dev'
+default_environment = f"{current_user}__{branch}".replace('-', '_')
+
+print(f"Environment is set to: {default_environment}.")
+
+config = Config(
+    project="obisidan-insights",
+    default_target_environment=default_environment,
+    gateways={
+            "duckdb": GatewayConfig(
+                connection=DuckDBConnectionConfig(
+                    database="tpch.duckdb"
+                )
+            ),
+        },
+    default_gateway="duckdb",
+    model_defaults=ModelDefaultsConfig(
+        dialect="duckdb,normalization_strategy=case_sensitive",
+        start="2025-01-01",
+        cron="*/5 * * * *"
+    ),
+    model_naming=NameInferenceConfig(
+        infer_names=True
+    ),
+    plan=PlanConfig(
+        auto_categorize_changes=CategorizerConfig(
+            external=AutoCategorizationMode.FULL,
+            python=AutoCategorizationMode.FULL,
+            sql=AutoCategorizationMode.FULL,
+            seed=AutoCategorizationMode.FULL
+        )
+    )
+)
