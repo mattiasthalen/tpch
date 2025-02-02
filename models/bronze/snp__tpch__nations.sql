9154d3ceb9e716d8b3a5877ad4493feb20c0016b
Mattias Thalén <bitter-polders0x@icloud.com>
2025-02-03 00:27:25 +0100

    Add generator sql, snapshots and external models

---
+++ models/bronze/snp__tpch__nations.sql
@@ -0,0 +1,11 @@
+MODEL (
+  kind INCREMENTAL_BY_UNIQUE_KEY (
+    unique_key _sqlmesh__hash_diff
+  )
+);
+
+SELECT
+  *,
+  @generate_surrogate_key(COLUMNS(*)) AS _sqlmesh__hash_diff,
+  @execution_ts::TIMESTAMP AS _sqlmesh__loaded_at
+FROM main.nation
