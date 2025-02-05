c1fc2535d1fe84fb85da461684d896e1cc1297f3
Mattias Thalén <bitter-polders0x@icloud.com>
2025-02-05 21:15:35 +0100

    Activate calendar

--- models/gold/calendar.py
+++ models/gold/calendar.py
@@ -205,7 +205,6 @@ def generate_calendar(df: pl.DataFrame, date_column: str) -> pl.DataFrame:
 
 @model(
     name='gold.calendar',
-    enabled=False,
     kind=dict(
         name=ModelKindName.FULL,
     ),
